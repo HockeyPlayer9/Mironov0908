4340b82b0c5722fe9f3f56cbe97ff9d6135729f6
kirillmironov9 <kirill.mironov93@yandex.ru>
2023-08-09 23:54:05 +0300

    add TestNG

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,24 @@
+import org.example.CashbackHackService;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void testRemain () {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainCash () {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+}
