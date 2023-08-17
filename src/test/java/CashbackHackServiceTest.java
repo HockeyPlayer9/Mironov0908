ddf3f420db7219119c4737f5f5bedbd3ff3587db
kirillmironov9 <kirill.mironov93@yandex.ru>
2023-08-17 21:55:39 +0300

    add test

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,7 +1,7 @@
 import org.example.CashbackHackService;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class CashbackHackServiceTest {
     @Test
